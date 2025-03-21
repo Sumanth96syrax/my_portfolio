61e3caa9a319ebb74f493ce4f2d34d5c717bda23
Sumanth96syrax <120739311+Sumanth96syrax@users.noreply.github.com>
2025-03-21 16:11:21 +0530

    Updated portfolio with new blog content

--- my_portfolio-master/src/component/About.js
+++ my_portfolio-master/src/component/About.js
@@ -10,7 +10,7 @@ function About() {
           <span className="abt">ABOUT ME</span> <br />
           <br />
           Hello, I'm <span className="bd">SUMANTH BRUNGI </span>and i'm Front-End Software Engineer. <br />I have a passion for creating responsive,
-          features rich website's with a greate user{" "}
+          features rich website's with a great user{" "}
           <span className="bd">INTERFACE. </span>
           <br />
           I'm currently looking for an <span className="bd">OPPORTUNITY </span>
--- my_portfolio-master/src/component/Button.js
+++ my_portfolio-master/src/component/Button.js
@@ -4,7 +4,7 @@ function Button({ text }) {
   return (
     <a
 
-      href="https://drive.google.com/file/d/10JbudYLBOkwUdX_-G5Pj9aD-UhAO8QEm/view?pli=1"
+      href="https://drive.google.com/file/d/1sZfGrvTTuvXxwsnIXaYMqIYXqRFqvvgo/view?pli=1"
         target="_blank"
     >
       <button className="btn">{text}</button>
